a0d76bd29a5c91a635a3ce48d806db291c2fa546
Badinage11 <147810289+Badinage11@users.noreply.github.com>
2024-11-07 21:28:26 +0800

    I have written the first part of the code for the first time

--- src/HealthProfessional.java
+++ src/HealthProfessional.java
@@ -1,2 +1,54 @@
 public class HealthProfessional {
-}
+        // Instance variables
+        private int id;
+        private String name;
+        private String specialization; // An additional relevant variable
+
+        // Default constructor
+        public HealthProfessional() {
+            this.id = 0;
+            this.name = "Unknown";
+            this.specialization = "General";
+        }
+
+        // Constructor that initializes all instance variables
+        public HealthProfessional(int id, String name, String specialization) {
+            this.id = id;
+            this.name = name;
+            this.specialization = specialization;
+        }
+
+        // Method to print all instance variables
+        public void printDetails() {
+            System.out.println("Health Professional Details:");
+            System.out.println("ID: " + id);
+            System.out.println("Name: " + name);
+            System.out.println("Specialization: " + specialization);
+        }
+
+        // Getters and setters (optional, but recommended for encapsulation)
+        public int getId() {
+            return id;
+        }
+
+        public void setId(int id) {
+            this.id = id;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public String getSpecialization() {
+            return specialization;
+        }
+
+        public void setSpecialization(String specialization) {
+            this.specialization = specialization;
+        }
+    }
+    
